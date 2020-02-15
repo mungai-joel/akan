865551843174a67c622d842214cd7a39917d3979
joel <mungaijoel123@gmail.com>
2020-02-15 15:01:00 +0300

    calculated the logic

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ function myInput(){
     var birthCentury = parseInt(document.getElementById("birthcentury").value);
     var birthMonth =parseInt(document.getElementById("birthmonth").value);
     var birthDate = parseInt(document.getElementById("birthdate").value);
-    var generate = (((birthCentury/4)-2*birthCentury-1)+((5*birthYear/4)) + ((26*(birthMonth+1)/10))+birthDate)%7;
+    var generate = parseInt(((birthCentury/4)-2*birthCentury-1)+((5*birthYear/4)) + ((26*(birthMonth+1)/10))+birthDate)%7;
     var gender1 = document.getElementById("gender");
     var gender2 = document.getElementById("gender1");
     var maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames =["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
     var day = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Sunday"]
-
-
+    var dateofbirth = Math.floor(generate);
+        alert(dateofbirth);
 }
